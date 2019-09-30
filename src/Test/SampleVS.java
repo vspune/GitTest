6ea0af766d5071bacf57ded6ec192b8e9798c1f3
Pawan Prajapati <Pawan.Prajapati@INDECOMM.LOCAL>
2019-09-30 14:24:54 +0530

    fouth commit

--- src/Test/SampleVS.java
+++ src/Test/SampleVS.java
@@ -5,6 +5,7 @@ public class SampleVS {
 	public static void main(String args[])
 	{
 		System.out.println("This is a sample program 3");
+		System.out.println("This is a sample program 2");
 		
 	}
 
