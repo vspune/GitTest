88c75f1554effbf6acbe78662bd070bdeba4ae1e
Vaibhav Sharma <Vaibhav.Sharma@INDECOMM.net>
2019-09-30 14:00:53 +0530

    first commit

--- src/Test/SampleVS.java
+++ src/Test/SampleVS.java
@@ -4,7 +4,7 @@ public class SampleVS {
 	
 	public static void main(String args[])
 	{
-		System.out.println("This is a sample program 1");
+		System.out.println("This is a sample program 3");
 		
 	}
 
