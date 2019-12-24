5ad04468dd26ba5e4d4695b5efd0ff292104c293
Pawan Prajapati <Pawan.Prajapati@indecomm.com>
2019-12-24 18:10:59 +0530

    modified file

--- src/Test/SampleVS.java
+++ src/Test/SampleVS.java
@@ -5,8 +5,9 @@ public class SampleVS {
 	public static void main(String args[])
 	{
 		System.out.println("This is a sample program 1");
-		System.out.println("Vaibhav here");
-		System.out.println("This is a sample 124124 program 3");
+		System.out.println("This is a sample program 2");
+		System.out.println("Vaibhav here 3");
+		System.out.println("This is a sample 124124 program 4");
 		
 	}
 
