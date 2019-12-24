d8094207f401321fe6b0766d1f8372695be7cec1
Vaibhav Sharma <Vaibhav.Sharma@indecomm.com>
2019-12-24 18:02:46 +0530

    removed 3 line

--- src/Test/SampleVS.java
+++ src/Test/SampleVS.java
@@ -5,7 +5,7 @@ public class SampleVS {
 	public static void main(String args[])
 	{
 		System.out.println("This is a sample program 1");
-		System.out.println("This is a sample program 2");
+		System.out.println("Vaibhav here");
 		System.out.println("This is a sample program 3");
 		
 	}
